15e8a492efeaa6c1cf7ebed31826ee637dce3ae9
jakebildy <jbildy@uwo.ca>
2018-02-20 18:33:12 -0500

    App frame completed

--- app/app/src/main/java/design1050/parabowapp/LoadingScreen.java
+++ app/app/src/main/java/design1050/parabowapp/LoadingScreen.java
@@ -1,7 +1,10 @@
 package design1050.parabowapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import static java.lang.Thread.sleep;
 
 public class LoadingScreen extends AppCompatActivity {
 
@@ -9,5 +12,29 @@ public class LoadingScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loading_screen);
+
+
+        final Intent intent = new Intent(this, MainActivity.class);
+
+        Thread thread = new Thread(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                try {
+                    sleep(3000);
+
+                    startActivity(intent);
+
+                }
+                catch (Exception e)
+                {}
+            }
+
+        });
+        thread.start();
     }
+
+
+
 }
---
+++ app/app/src/main/java/design1050/parabowapp/MainActivity.java
@@ -0,0 +1,18 @@
+package design1050.parabowapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.VideoView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+     //   VideoView view = findViewById(R.id.videoView);
+
+     //   view.setVideoPath("https://youtu.be/1vAHn50KLuI?t=16");
+    }
+}
