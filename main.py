db16bd5b0f2735a2c1b0b88661213eb8a0f41a33
jakebildy <jbildy@uwo.ca>
2018-02-22 17:20:15 +0000

    Implementing pygame for sound

--- main.py
+++ main.py
@@ -3,7 +3,22 @@
 import requests
 import RPi.GPIO as GPIO
 import time 
+import pygame
 
-r = requests.get('')
 
-print(r.text)
+
+r = requests.get('https://google.ca')
+
+#print(r.text)
+
+
+pygame.mixer.init()
+pygame.mixer.music.load("firing.wav")
+pygame.mixer.music.play()
+
+while pygame.mixer.music.get_busy() == True:
+	continue 
+
+
+
+
