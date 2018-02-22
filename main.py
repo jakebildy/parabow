23661ef2b5092a8991dc408f96d0a40b9c2a2776
jakebildy <jbildy@uwo.ca>
2018-02-22 18:02:59 +0000

    Testing omxplayer

--- main.py
+++ main.py
@@ -3,22 +3,16 @@
 import requests
 import RPi.GPIO as GPIO
 import time 
-import pygame
-
-
+import os
 
 r = requests.get('https://google.ca')
 
 #print(r.text)
 
+for i in range(3):
 
-pygame.mixer.init()
-pygame.mixer.music.load("firing.wav")
-pygame.mixer.music.play()
+	time.sleep(3)
 
-while pygame.mixer.music.get_busy() == True:
-	continue 
-
-
+	os.system("omxplayer -o local firing.wav")
 
 
