e31a1919402edf64b8b1f693e86666236a114e8f
Jake Bildy <33790840+jakebildy@users.noreply.github.com>
2018-03-28 00:17:51 -0400

    Update main.py

--- main.py
+++ main.py
@@ -10,6 +10,14 @@ server_socket = BluetoothSocket( RFCOMM )
 server_socket.bind(("",PORT_ANY))
 server_socket.listen(1)
 
+uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
+
+advertise_service( server_socket, "ParabowServer",
+                   service_id = uuid,
+                   service_classes = [ uuid, SERIAL_PORT_CLASS ],
+                   profiles = [ SERIAL_PORT_PROFILE ]
+                    )
+
 client_socket,address = server_socket.accept()
 print ("Connected to " + address + " lmaoo")
 
