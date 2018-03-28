f86809a3065aa10bfcea41b3038e2d4a3e842cae
jakebildy <jbildy@uwo.ca>
2018-03-28 07:19:23 +0000

    Linked pi with android

--- main.py
+++ main.py
@@ -4,7 +4,7 @@
 import RPi.GPIO as GPIO
 import time 
 import os
-import bluetooth
+from bluetooth import *
 
 server_socket = BluetoothSocket( RFCOMM )
 server_socket.bind(("",PORT_ANY))
@@ -12,23 +12,19 @@ server_socket.listen(1)
 
 uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
 
-advertise_service( server_socket, "ParabowServer",
-                   service_id = uuid,
-                   service_classes = [ uuid, SERIAL_PORT_CLASS ],
-                   profiles = [ SERIAL_PORT_PROFILE ]
-                    )
+advertise_service(server_socket, "ParabowServ2", service_id=uuid,service_classes=[uuid, SERIAL_PORT_CLASS],profiles=[SERIAL_PORT_PROFILE])
+
+print("Preparing to connect")
 
 client_socket,address = server_socket.accept()
-print ("Connected to " + address + " lmaoo")
 
-while 1:
+print ("Connected lmaoo")
+
+while (True) :	
 	data = client_socket.recv(1024)
-	if data == "fire" :
+	if len(data) > 0 :
+		print ("firing...")
 		os.system("omxplayer -o local firing.wav")
-	
-client_socket.close()
-server_socket.close() 
-
-
+		time.sleep(2)
 
 
