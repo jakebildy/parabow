bcf291f2babf845b005b15d35a1e3b108580da90
jakebildy <jbildy@uwo.ca>
2018-03-28 04:15:11 +0000

    switching to python2

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 #Developed by Jake Bildy, 2018
 
-import requests
+
 import RPi.GPIO as GPIO
 import time 
 import os
-import Bluetooth
+import bluetooth
 
-server_socket=bluetooth.BluetoothSocket(bluetooth.RFCOMM)
-
-port = 1
+server_socket = BluetoothSocket( RFCOMM )
+server_socket.bind(("",PORT_ANY))
 server_socket.listen(1)
 
 client_socket,address = server_socket.accept()
